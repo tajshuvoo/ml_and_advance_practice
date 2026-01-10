52f48b0ebe9533f19b986d438e40e365731264d3
tajshuvoo <tajshuvo75@gmail.com>
2026-01-11 00:03:59 +0600

    prompt in langchain

---
+++ gen_ai_with_LangChain/langchain_prompts/prompt_ui.py
@@ -0,0 +1,38 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_core.prompts import PromptTemplate
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+st.header("Research Tool")
+
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+template = PromptTemplate(
+    template="""
+    \nPlease summarize the research paper titled \"{paper_input}\" with the following specifications:\nExplanation Style: {style_input}  \nExplanation Length: {length_input}  \n1. Mathematical Details:  \n   - Include relevant mathematical equations if present in the paper.  \n   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  \n2. Analogies:  \n   - Use relatable analogies to simplify complex ideas.  \nIf certain information is not available in the paper, respond with: \"Insufficient information available\" instead of guessing.  \nEnsure the summary is clear, accurate, and aligned with the provided style and length.\n
+    """,
+    input_variables=['paper_input', 'style_input', 'length_input'],
+    
+)
+
+prompt = template.invoke({
+    'paper_input' : paper_input,
+    'style_input' : style_input,
+    'length_input': length_input
+})
+
+if st.button('summerize'):
+    result = model.invoke(prompt)
+    st.write(result.content)

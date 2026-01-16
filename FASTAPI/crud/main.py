3b3baf1b7f28baa7981bf902ac30ec339d0009f2
tajshuvoo <tajshuvo75@gmail.com>
2026-01-16 12:33:24 +0600

    fast api path and query param

--- FASTAPI/crud/main.py
+++ FASTAPI/crud/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException,Query
 import json
 
 app = FastAPI()
@@ -22,3 +22,31 @@ def view():
     data = load_data()
     
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str= Path(..., description='Id of the patient in the dbms', example='P001')):
+    data = load_data()
+    
+    if patient_id in data:
+        return data[patient_id]
+    
+    raise HTTPException(status_code=404, detail='Patient not found!')
+
+@app.get('/sort')
+def sort_patient(sort_by: str= Query(..., description='Sort on the basis of hieght, weight or bmi'), 
+                 order: str = Query(..., description='sort in asc or desc order')):
+    valid_fields = ['height','weight','bmi']
+    
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail='Invalid field select from {valid_fields}')
+    
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400, detail='Invalid order selection')
+    
+    data = load_data()
+    
+    sort_order = True if order=='desc' else False
+    sorted_data = sorted(data.values(), key= lambda x:x.get(sort_by,0), reverse=sort_order)
+    
+    return sorted_data
+    
